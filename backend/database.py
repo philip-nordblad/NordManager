8576b91a44b2724e803f375bde4fd518be362222
philip.a.nordblad@gmail.com <philip.a.nordblad@gmail.com>
2024-12-24 11:15:44 -0800

    readjusting project, trial 2

--- backend/database.py
+++ backend/database.py
@@ -8,7 +8,7 @@ def get_db_connection():
 def init_db():
     conn = get_db_connection()
     with conn:
-        conn.execute("CREATE TABLE IF NOT EXISTS expenses (id INTEGER PRIMARY KEY, name TEXT, amount REAL)")
+        conn.execute("CREATE TABLE IF NOT EXISTS expenses (id INTEGER PRIMARY KEY, title TEXT, amount REAL)")
         conn.execute("CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, name TEXT, completed BOOLEAN)")
         conn.execute("CREATE TABLE IF NOT EXISTS events (id INTEGER PRIMARY KEY, name TEXT, date TEXT)")
     conn.close()
